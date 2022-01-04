a7de96a29c7ccd0941aa373e088f27510b75c8a1
motohiro.ito <uthitt.1216.i.m@gmail.com>
2022-01-04 15:06:42 +0000

    top page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,32 +2,20 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>TaskList</title>
+        <title>Microposts</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     </head>
 
     <body>
-        <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                {{-- トップページへのリンク --}}
-                <a class="navbar-brand" href="/">TaskList</a>
 
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item"></li>{!! link_to_route('tasks.create', '新しいタスクの追加', [], ['class'=> 'nav-link']) !!}</li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        {{-- ナビゲーションバー --}}
+        @include('commons.navbar')
 
         <div class="container">
+            {{-- エラーメッセージ --}}
             @include('commons.error_tasks')
+
             @yield('content')
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', 'TasksController@index');
-
-Route::resource('tasks', 'TasksController');
+Route::get('/', function() {return view('welcome');});
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
